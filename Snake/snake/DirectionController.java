7124fedd7db0f8b0a46dc5eeb96ceb84a3852fc9
hyorimlee <hl1046@pusan.ac.kr>
2020-06-07 10:38:29 +0900

    revise name

--- Snake/snake/DirectionController.java
+++ Snake/snake/DirectionController.java
@@ -5,16 +5,25 @@ public class DirectionController {
 	public DirectionController() {
 		this.direction = direction;
 	}
-	private static void set_behavoir(Direction nextMove,GameBoard gameBoard) {
+	private static void set_behavior(Direction nextMove,GameBoard gameBoard) {
 		if(nextMove==Direction.LEFT) gameBoard.set_behavior(new LeftBehavior());
 		if(nextMove==Direction.RIGHT) gameBoard.set_behavior(new RightBehavior());
 		if(nextMove==Direction.UP) gameBoard.set_behavior(new UpBehavior());
 		if(nextMove==Direction.DOWN) gameBoard.set_behavior(new DownBehavior());
 	}
+		
+	static Direction reverse(Direction direction) {
+		Direction rev = null;
+		if(direction ==Direction.LEFT) rev = Direction.RIGHT;
+		if(direction ==Direction.RIGHT) rev = Direction.LEFT;
+		if(direction ==Direction.UP) rev = Direction.DOWN;
+		if(direction ==Direction.DOWN) rev = Direction.UP;
+		return rev;
+	}
 	public static void setDirection(GameBoard gameBoard,Direction lastMove,Direction nextMove) {
-		if(lastMove!=gameBoard.reverse(nextMove)||gameBoard.getSnakeSize()==1){
+		if(lastMove!=reverse(nextMove)||gameBoard.getSnakeSize()==1){
 		gameBoard.set_movement(nextMove);
-		set_behavoir(nextMove,gameBoard);
+		set_behavior(nextMove,gameBoard);
 		}
 	}
 }
