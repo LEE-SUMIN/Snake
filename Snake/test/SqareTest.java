4815e5f2ac1ec72c9abdc8315232de2c44570a12
Seonghwan Hwang <shwanh0918@gmail.com>
2020-06-20 00:20:13 +0900

    Test Case_2
    whitebox test(statement coverage) - Success

--- Snake/test/SqareTest.java
+++ Snake/test/SqareTest.java
@@ -8,15 +8,27 @@ public class SqareTest {
 	private Square square;
 	
 	/*
-	 * Purpose : Create Square with parameters and check its fields
+	 * Purpose : Create Square with Entity and check its fields
 	 * Expected : All checks will pass true
 	 */
 	@Test
-	public void testCreate() {
+	public void testCreateWithEntity() {
 		square=new Square(Entity.Empty,5,5);
 		assertEquals(Entity.Empty,square.getEntity());
 		assertEquals(5,square.getX());
 		assertEquals(5,square.getY());
+	}
+	
+	/*
+	 * Purpose : Create Square without Entity ,and Set and check Entity.
+	 * Expected : All checks will pass true
+	 */
+	@Test
+	public void testCreateWithoutEntityAndSetEntity() {
+		square=new Square(5,5);
+		assertEquals(Entity.Empty,square.getEntity());
+		square.setEntity(Entity.Food);
+		assertEquals(Entity.Food,square.getEntity());
 	}
 	
 }
