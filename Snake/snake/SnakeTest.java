1a177ae006ab4c596dc6f4ed5263a76ca6b58b98
hyorimlee <hl1046@pusan.ac.kr>
2020-06-21 19:47:48 +0900

    Update SnakeTest.java
    *Purpose: check when user press the moveKey(LEFT,RIGHT,UP,DOWN), really snake move that direction.
    	*Input: LEFT
    	*Expected:
    	*	Return True
    	*	X = old X -1
    	*	Y = old Y
    	*
    	*Input: RIGHT
    	*Expected:
    	*	Return True
    	*	X = old X +1
    	*	Y = old Y
    	*
    	**Input: UP
    	*Expected:
    	*	Return True
    	*	X = old X
    	*	Y = old Y-1
    	*
    	**Input: DOWN
    	*Expected:
    	*	Return True
    	*	X = old X
    	*	Y = old Y+1

--- Snake/snake/SnakeTest.java
+++ Snake/snake/SnakeTest.java
@@ -16,5 +16,55 @@ public class SnakeTest {
 		Snake snake = new Snake();
 	 	assertTrue(snake.getSize()==1);
 	}
+	/**
+	*Purpose: check when user press the moveKey(LEFT,RIGHT,UP,DOWN), really snake move that direction.
+	*Input: LEFT
+	*Expected:
+	*	Return True
+	*	X = old X -1
+	*	Y = old Y
+	*
+	*Input: RIGHT
+	*Expected:
+	*	Return True
+	*	X = old X +1
+	*	Y = old Y
+	*
+	**Input: UP
+	*Expected:
+	*	Return True
+	*	X = old X
+	*	Y = old Y-1
+	*
+	**Input: DOWN
+	*Expected:
+	*	Return True
+	*	X = old X
+	*	Y = old Y+1
+	*
+	*/
+	@Test
+	public void testTurnSnake() {
+		Snake snake = new Snake();
+		Square pos = snake.getHead();
+		snake.moveLeft();
+		assertTrue(snake.getHead().getX()==pos.getX()-1);
+		assertTrue(snake.getHead().getY()==pos.getY());
+		
+		pos = snake.getHead();
+		snake.moveRight();
+		assertTrue(snake.getHead().getX()==pos.getX()+1);
+		assertTrue(snake.getHead().getY()==pos.getY());
+		
+		pos = snake.getHead();
+		snake.moveUp();
+		assertTrue(snake.getHead().getX()==pos.getX());
+		assertTrue(snake.getHead().getY()==pos.getY()-1);
+		
+		pos = snake.getHead();
+		snake.moveDown();
+		assertTrue(snake.getHead().getX()==pos.getX());
+		assertTrue(snake.getHead().getY()==pos.getY()+1);
+	}
 	
 }
