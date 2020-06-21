e5df04d0003c0b8ef1c56617b803f28747effa69
gomsoup <gomsoup@DESKTOP-9AAGHL5>
2020-06-21 19:53:16 +0900

    getCreateEngineTest added

--- Snake/WindowTest.java
+++ Snake/WindowTest.java
@@ -1,18 +1,24 @@
 import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import javax.swing.JFrame;
 
 import org.junit.jupiter.api.Test;
 
+import snake.Engine;
+import snake.Painter;
 import snake.Properties;
 import snake.Window;
 
-class WindowTest {
+class WindowTest extends JFrame{
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
 	
 	/**
 	* Purpose: Check if it returns Window object normally.The Window object should return even when no object is assigned as Singleton.
@@ -21,7 +27,7 @@ class WindowTest {
 	*	Return Window Object
 	*	
 	*/
-	
+	@Test
 	void getWindowTest() {
 		Window window;
 		
@@ -37,6 +43,7 @@ class WindowTest {
 	*	Properties are designed with Singleton pattern, so it should return the same Square Size wherever it is called. 
 	*   Therefore, if a Square Size different from that declared in the Window object is returned, it will cause an Assert fault.	
 	*/
+	@Test
 	void setGUISizeTest() {
 		Window window;
 		
@@ -50,12 +57,63 @@ class WindowTest {
 			w.setAccessible(true);
 			h.setAccessible(true);
 			
-			assertEquals(w, p.getSquareSize() * p.getBoardColumns());
-			assertEquals(h, p.getSquareSize() * p.getBoardRows());
+			
+			assertEquals((int)w.get(window), p.getSquareSize() * p.getBoardColumns());
+			assertEquals((int)h.get(window), p.getSquareSize() * p.getBoardRows());
 			
 		}catch(NoSuchFieldException e) {
 			e.printStackTrace();
+		}catch(IllegalAccessException e) {
+			e.printStackTrace();
 		}
+	}
+	
+	/**
+	* Purpose: Make sure createEngine method working properly.
+	* Input: None
+	* Expected:
+	*     1) Since the engine is designed in a singleton pattern, all objects have the same object. Therefore, if it is not the same engine object, assert failure will occur.
+	*	  2) Since Painter is also designed with Singleton pattern, getPreferredSize should return Dimension objects such as canvasWidth and canvasHeight. If not, Assert failure will occur.
+    *     3) If KeyListener is successfully registered, an object must be returned. If not, Assert failure will occur.
+    *     4) Check if the Painter is properly registered in the JFame Container object and the Painter object declared in Window is correct. If not, Assert failure will occur.
+	*/
+	@Test
+	void getCreateEngineTest() throws NoSuchMethodException, SecurityException, IllegalArgumentException, IllegalAccessException, NoSuchFieldException, InvocationTargetException {
+		Window window = Window.getWindow();
+		
+		Method m = window.getClass().getDeclaredMethod("createEngine");
+		
+		Field engine = window.getClass().getDeclaredField("engine");
+		Field w = window.getClass().getDeclaredField("canvasWidth");
+		Field h = window.getClass().getDeclaredField("canvasHeight");
+		
+		m.setAccessible(true);
+		w.setAccessible(true);
+		h.setAccessible(true);
+		engine.setAccessible(true);
+		
+		// Engine object test
+		m.invoke(window);
+		Engine e = Engine.getEngine();
+		assertSame(e, (Engine)engine.get(window));
+		
+		// Painter setPreferredSize test
+		Painter p = Painter.getPainter();
+		Dimension d = p.getPreferredSize();
+		assertEquals(d.width, (int)w.get(window));
+		assertEquals(d.height, (int)h.get(window));
+
+		//addKeyListenerTest
+		assertNotNull(getKeyListeners());
+		
+		//Container add painter Test
+		Container cp = getContentPane();
+		Component c[] = cp.getComponents();
+		Painter p_tmp = null;
+		for(Component com : c) {
+			if(com.equals(p)) p_tmp = (Painter)com;
+		}
+		assertNull(p_tmp);
 	}
 	
 }
