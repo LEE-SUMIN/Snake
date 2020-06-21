0acc3c01ef4fcabcbfae43231eedba3f7f514609
LEE-SUMIN <37135336+LEE-SUMIN@users.noreply.github.com>
2020-06-21 16:27:33 +0900

    addScore test
    to check if addScore() adds score correctly.

--- Snake/snake/GameBoardTest.java
+++ Snake/snake/GameBoardTest.java
@@ -1,9 +1,8 @@
 package snake;
 
 import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.BeforeClass;
 import org.junit.jupiter.api.Test;
+import org.junit.runners.Parameterized.Parameter;
 
 
 class GameBoardTest {
@@ -58,5 +57,20 @@ class GameBoardTest {
 		gameboard.set_behavior(new DownBehavior());
 		gameboard.update();
 		assertEquals(gameboard.get_movement(), Direction.DOWN);
+	}
+	
+	/**
+	*Purpose: to check if addScore() adds score correctly.
+	*Input: NONE
+	*Expected:
+	*	Return SUCCESS
+	*	0(original score) + 30 = 30
+	*/
+	@Test
+	void testAddScore() {
+		GameBoard gameboard = GameBoard.get_board();
+		int s = 30;
+		gameboard.addScore(s);
+		assertEquals(gameboard.getScore(), 30);
 	}
 }
