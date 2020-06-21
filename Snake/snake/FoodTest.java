07e1a26721524bbb894d9e8cdb83dabe7693361c
LEE-SUMIN <37135336+LEE-SUMIN@users.noreply.github.com>
2020-06-21 17:38:21 +0900

    setfood test
    to check if the snake does not contain the food

--- Snake/snake/FoodTest.java
+++ Snake/snake/FoodTest.java
@@ -18,5 +18,18 @@ class FoodTest {
 		assertNotNull(food);
 	}
 
-
+	/**
+	*Purpose: to check if the snake does not contain the food.
+	*Input:
+	*Expected:
+	*	Return SUCCESS
+	*	if the snake holds food, it has to generate food again.
+	*   Therefore, the food generated finally must not be inside the snake.
+	*/
+	@Test
+	void testSetFoodContain() {
+		Snake snake = Snake.get_snake();
+		Food food = new Food();
+		assertFalse(snake.contains(food.get_food()));
+	}
 }
