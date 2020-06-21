fa56775fffca772da69ab960a929d018d8bfe833
gomsoup <gomsoup@DESKTOP-9AAGHL5>
2020-06-21 21:36:20 +0900

    keyInterruptHandlerEngineRunningDecisionTest added

--- Snake/EngineTest.java
+++ Snake/EngineTest.java
@@ -75,4 +75,44 @@ class EngineTest {
 		e.keyInterruptHandler(KeyEvent.VK_F1);
 		assertEquals(Theme.Dark, (Theme)t.get(p));
 	}
+	
+	/**
+	* Purpose: Run a branch test of the keyInterruptHandler method. 
+	* 		   Make sure to execute the proper syntax for each branch condition.
+	* Input: F1, RIGHT, Up Key Input
+	* Expected:
+	*     If the game is not running and a function key is entered, only the theme is changed and the running bit should not be affected. Otherwise, assert failure occurs.
+	*     If the direction key is input when the running bit is not set, the game should start. Otherwise, assert failure occurs.
+	*     The direction of the snake should be set according to the direction key. Otherwise, assert failure occurs.
+	*/
+	@Test
+	void keyInterruptHandlerEngineRunningDecisionTest() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+		Engine.initialize();
+		Engine e = Engine.getEngine();
+		GameBoard g = GameBoard.get_board();
+		Properties p = Properties.Instance();
+		
+		Field running = e.getClass().getDeclaredField("running");
+		running.setAccessible(true);
+		assertFalse(running.getBoolean(e));
+		
+		Field t = p.getClass().getDeclaredField("theme");
+		t.setAccessible(true);
+		e.keyInterruptHandler(KeyEvent.VK_F1);
+		assertFalse(running.getBoolean(e));
+		assertEquals(Theme.Dark, (Theme)t.get(p));
+		
+		e.keyInterruptHandler(KeyEvent.VK_RIGHT);
+		assertTrue(running.getBoolean(e));
+		
+		Field m = g.getClass().getDeclaredField("movement");
+		m.setAccessible(true);
+		e.keyInterruptHandler(KeyEvent.VK_UP);
+		assertEquals(Direction.UP, (Direction)m.get(g));
+		assertTrue(running.getBoolean(e));
+		
+		e.keyInterruptHandler(KeyEvent.VK_RIGHT);
+		assertEquals(Direction.RIGHT, (Direction)m.get(g));
+		assertTrue(running.getBoolean(e));
+	}
 }
